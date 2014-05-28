623568a0bcf78e8afb64e6e2ed1744349e2c572a
Tom Dooner <tom.dooner@causes.com>
2014-05-28 16:03:11 -0700

    Add some better responsiveness

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'sinatra'
 ENV['DATABASE_URL'] ||= 'sqlite3://./db/development.sqlite3'
 ActiveRecord::Base.establish_connection
 
+configure do
+  set :static_root, '/public'
+end
+
 get '/' do
   haml :index
 end
