e450df9afa293800b3d6debdacf16370cd7929e9
Tom Dooner <tom.dooner@causes.com>
2014-05-29 15:05:16 -0400

    Bugfix with cover Photo and styling tweaks

--- models/photo.rb
+++ models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :petition
+
+  before_save :set_as_cover_photo, if: -> { petition.photos.blank? }
+
+  def set_as_cover_photo
+    self.cover = true
+  end
 end
